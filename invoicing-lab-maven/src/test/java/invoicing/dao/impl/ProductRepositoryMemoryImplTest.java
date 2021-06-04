48c30d62fb77e501093824926452ce53610794e7
Trayan Iliev <office@iproduct.org>
2021-06-04 09:22:57 +0300

    presentation added

--- invoicing-lab-maven/src/test/java/invoicing/dao/impl/ProductRepositoryMemoryImplTest.java
+++ invoicing-lab-maven/src/test/java/invoicing/dao/impl/ProductRepositoryMemoryImplTest.java
@@ -7,10 +7,19 @@ import invoicing.model.Unit;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
 
 @Slf4j
 class ProductRepositoryMemoryImplTest {
@@ -38,6 +47,7 @@ class ProductRepositoryMemoryImplTest {
 
     @BeforeEach
     void init() {
+        log.info("@BeforeEach - executes before each test method in this class");
         repo = new ProductRepositoryMemoryImpl(new LongKeyGenerator());
         SAMPLE_PRODUCTS.forEach(p -> {
             try {
@@ -46,8 +56,17 @@ class ProductRepositoryMemoryImplTest {
                 e.printStackTrace();
             }
         });
-        log.info("@BeforeEach - executes before each test method in this class");
     }
+
+//    @BeforeEach
+//    void beforeEach(TestInfo testInfo, RepetitionInfo repetitionInfo) {
+//        int currentRepetition = repetitionInfo.getCurrentRepetition();
+//        int totalRepetitions = repetitionInfo.getTotalRepetitions();
+//        String methodName = testInfo.getTestMethod().get().getName();
+//
+//        System.out.println(String.format("About to execute repetition %d of %d for %s", //
+//                currentRepetition, totalRepetitions, methodName));
+//    }
 
     @AfterEach
     void tearDown() {
@@ -55,8 +74,13 @@ class ProductRepositoryMemoryImplTest {
     }
 
     @Test
+    @DisplayName("Find product by ID")
     void findById() throws EntityAlreadyExistsException {
-        assertEquals(repo.create(NEW_PRODUCT).getCode()s, "CB001");
+        Product result = repo.create(NEW_PRODUCT);
+        assertNotNull(result);
+        assertNotNull(result.getId());
+        assertEquals(result.getCode(), "CB001");
+
     }
 
     @Test
@@ -65,13 +89,13 @@ class ProductRepositoryMemoryImplTest {
         List<Product> result = repo.findAll();
         SoftAssertions softly = new SoftAssertions();
         softly.assertThat(softly.assertThat(result)).isNotNull();
-        softly.assertThat(result.size()).isEqualTo(5);
+        softly.assertThat(result).hasSize(5);
         softly.assertThat(result.get(0).getCode()).isEqualTo("BK001");
         softly.assertAll();
     }
 
     @Test
-    @Disabled("Not implemented yet")
+    @Disabled("Not yet implemented")
     void create() {
     }
 
@@ -85,5 +109,115 @@ class ProductRepositoryMemoryImplTest {
 
     @Test
     void count() {
+    }
+
+    @Test
+    void assumptionThat() {
+        String someString = "Some string";
+        assumingThat(
+                someString.equals("Some string"),
+                () -> assertEquals(11, someString.length())
+        );
+    }
+
+    @Test
+    void shouldThrowException() {
+        Throwable exception = assertThrows(UnsupportedOperationException.class, () -> {
+            throw new UnsupportedOperationException("Not supported");
+        });
+        assertEquals(exception.getMessage(), "Not supported");
+    }
+
+    @ParameterizedTest(name="#{index} - Test with Argument={0}")
+    @ValueSource(ints = {8,4,2,6,10})
+    void test_int_arrays(int arg) {
+        System.out.println("arg => "+arg);
+        assertTrue(arg % 2 == 0);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "Peter, admin, 1",
+            "John, author, 2",
+            "Martin, subscriber, 3"
+    })
+    void testWith_CsvSource(String name, String role, long id) {
+        System.out.println("testWith_CsvSource: name => "+name+"; role => "+role+"; id => "+id);
+        assertTrue(name.length() >= 0);
+        assertTrue(id >=1 && id <=3);
+        assertTrue(!role.isEmpty());
+    }
+
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/users-data.csv", numLinesToSkip = 1)
+    void testWith_MethodSource(String name, String role, long id) {
+        System.out.println("name => "+name+"; role => "+role+"; id => "+id);
+        assertTrue(name.length() >= 0);
+        assertTrue(id >=1 && id <=3);
+        assertTrue(!role.isEmpty());
+    }
+
+    @RepeatedTest(3)
+    void test_Devide() {
+        System.out.println("test_Devide()");
+        assertEquals(5, 25 /5);
+    }
+
+    @TestFactory
+    Collection<DynamicTest> dynamicTestsCollection() {
+        return Arrays.asList(
+                DynamicTest.dynamicTest("Add test",
+                        () -> assertEquals(5, Math.addExact(2, 3))),
+                DynamicTest.dynamicTest("Multiply Test",
+                        () -> assertEquals(15, Math.multiplyExact(5, 3))));
+    }
+
+    @TestFactory
+    Stream<DynamicTest> dynamicTestsStream() {
+        return IntStream.iterate(0, n -> n + 5).limit(10)
+                .mapToObj(n -> DynamicTest.dynamicTest("testMultipleOfFive_" + n,
+                        () -> assertTrue(n % 5 == 0)));
+    }
+
+    @TestFactory
+    Stream<DynamicTest> dynamicTestsForProductWorkflows() {
+        List<Product> inputList = Arrays.asList(
+                new Product("AC001", "Mouse", 12.5),
+                new Product("AC002", "Headphones", 25.7),
+                new Product("AC003", "keyboard", "high quality wireless keyboard", 29.45));
+
+        Stream<DynamicTest> saveProductStream = inputList.stream()
+                .map(product -> DynamicTest.dynamicTest(
+                        "createProduct: " + product.toString(),
+                        () -> {
+                            Product returned = repo.create(product);
+                            assertEquals(returned.getCode(), product.getCode());
+                            assertEquals(returned.getName(), product.getName());
+                            assertEquals(returned.getPrice(), product.getPrice());
+                        }
+                ));
+
+        List<Product> inputList2 = Arrays.asList(
+                new Product("AC001", "Mouse", 12.5),
+                new Product("AC002", "Headphones", 25.7),
+                new Product("AC003", "keyboard", "high quality wireless keyboard", 29.45));
+        inputList.get(2).setId(1000L);
+
+        try (Stream<DynamicTest> saveProductWithFirstNameStream = inputList2.stream()
+                .filter(product -> product.getId() != null)
+                .map(product -> DynamicTest.dynamicTest(
+                        "createProductWithId" + product.toString(),
+                        () -> {
+                            Product returned = repo.create(product);
+                            assertEquals(returned.getId(), product.getId());
+                            assertEquals(returned.getCode(), product.getCode());
+                            assertEquals(returned.getName(), product.getName());
+                            assertEquals(returned.getPrice(), product.getPrice());
+                        }))) {
+
+            return Stream.concat(saveProductStream,
+                    saveProductWithFirstNameStream);
+        }
     }
 }
