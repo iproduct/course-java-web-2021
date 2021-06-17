0cd71e0ddca4f14b149bc03323830bc932b93c2f
Trayan Iliev <office@iproduct.org>
2021-06-17 09:32:03 +0300

    var -> ConfigurableApplicationcontext

--- hello-spring-mvc-gradle/src/main/java/invoicing/HelloSpringMvcGradleApplication.java
+++ hello-spring-mvc-gradle/src/main/java/invoicing/HelloSpringMvcGradleApplication.java
@@ -2,12 +2,13 @@ package invoicing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class HelloSpringMvcGradleApplication {
 
 	public static void main(String[] args) {
-		var ctx = SpringApplication.run(HelloSpringMvcGradleApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(HelloSpringMvcGradleApplication.class, args);
 		ctx.registerShutdownHook();
 	}
 
