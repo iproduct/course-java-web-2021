2af05da75b7bdacfc3f00d158edac44d68588454
Trayan Iliev <office@iproduct.org>
2021-06-21 17:25:18 +0300

    JPA -> MongoDB project conversion

---
+++ invoicing-spring-mvc-mongo/src/main/java/invoicing/config/WebConfig.java
@@ -0,0 +1,14 @@
+package invoicing.config;
+
+import invoicing.web.ProfilerInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new ProfilerInterceptor());
+    }
+}
---
+++ invoicing-spring-mvc-mongo/src/main/java/invoicing/web/ProfilerInterceptor.java
@@ -0,0 +1,25 @@
+package invoicing.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+public class ProfilerInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        long startTime = System.nanoTime();
+        request.setAttribute("startTime", startTime);
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        long endTime = System.nanoTime();
+        long startTime = (Long) request.getAttribute("startTime");
+        log.info("[{}] executed in: {} ms", handler, endTime-startTime / 1e6);
+    }
+}
