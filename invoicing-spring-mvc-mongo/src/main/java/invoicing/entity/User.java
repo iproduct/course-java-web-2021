7c0d7617940f1bbc7e17b679f5817d19d81794e7
Trayan Iliev <office@iproduct.org>
2021-06-22 12:52:51 +0300

    mock mvc tests added

--- invoicing-spring-mvc-mongo/src/main/java/invoicing/entity/User.java
+++ invoicing-spring-mvc-mongo/src/main/java/invoicing/entity/User.java
@@ -24,7 +24,7 @@ public class User implements UserDetails {
     private String firstName; // string 2 to 20 characters long;
     private String lastName; // string 2 to 20 characters long;
     private String username; // 2 to 15 characters long - word characters only, unique within the system, cannot be changed;
-    @JsonProperty(access= WRITE_ONLY)
+//    @JsonProperty(access = WRITE_ONLY)
     private String password; // string 8 to 15 characters long, at least one digit, one capital letter, and one sign different than letter or digit, NOT sent back to the User clients for security reasons;
     private Role role = USER; // USER or ADMIN enumeration, USER by default, editable only by Administrators;
     private boolean active = true; // validity status of the user account true by default;
@@ -41,6 +41,16 @@ public class User implements UserDetails {
         this.lastName = lastName;
         this.username = username;
         this.password = password;
+    }
+
+    public User(String id, String firstName, String lastName, String username, String password, Role role, boolean active) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.active = active;
     }
 
     public User(String firstName, String lastName, String username, String password, Role role, boolean active) {
@@ -113,11 +123,9 @@ public class User implements UserDetails {
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return Set.of(new SimpleGrantedAuthority("ROLE_" + role));
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
@@ -136,6 +144,34 @@ public class User implements UserDetails {
 
     public void setActive(boolean active) {
         this.active = active;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+
+        User user = (User) o;
+
+        if (active != user.active) return false;
+        if (id != null ? !id.equals(user.id) : user.id != null) return false;
+        if (firstName != null ? !firstName.equals(user.firstName) : user.firstName != null) return false;
+        if (lastName != null ? !lastName.equals(user.lastName) : user.lastName != null) return false;
+        if (username != null ? !username.equals(user.username) : user.username != null) return false;
+        if (password != null ? !password.equals(user.password) : user.password != null) return false;
+        return role == user.role;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (username != null ? username.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (role != null ? role.hashCode() : 0);
+        result = 31 * result + (active ? 1 : 0);
+        return result;
     }
 
     @Override
--- invoicing-spring-mvc-mongo/src/test/java/invoicing/EinvoiceGradleApplicationTests.java
+++ invoicing-spring-mvc-mongo/src/test/java/invoicing/EinvoiceGradleApplicationTests.java
@@ -1,11 +1,39 @@
 package invoicing;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import invoicing.dao.UserRepository;
+import invoicing.entity.User;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static invoicing.entity.Role.ADMIN;
+import static invoicing.entity.Role.USER;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK,
@@ -13,9 +41,71 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @AutoConfigureMockMvc
 @Slf4j
 class EinvoiceGradleApplicationTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @Autowired
+    private ObjectMapper mapper;
 
     @Test
     void contextLoads() {
     }
+
+    @Test
+    void givenUsers_whenGetAllUsers_thenStatus200andJsonArray() throws Exception {
+        given(userRepository.findAll()).willReturn(SAMPLE_USERS);
+
+        MvcResult mvcResult = mockMvc.perform(get("/api/users")
+                .with(user("admin").password("admin123").roles(ADMIN.toString()))
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andDo(result -> log.info(result.getResponse().getContentAsString()))
+                .andExpect(jsonPath("$[0].username").value("admin"))
+//                .andExpect(jsonPath("$[0].password").doesNotExist())
+                .andExpect(jsonPath("$[1].username").value("user"))
+//                .andExpect(jsonPath("$[1].password").doesNotExist())
+                .andExpect(jsonPath("$[2].username").value("ivan"))
+//                .andExpect(jsonPath("$[2].password").doesNotExist())
+                .andReturn();
+        String body = mvcResult.getResponse().getContentAsString();
+        List<User> users = mapper.readValue(body, new TypeReference<List<User>>() {
+        });
+        List<User> usersWithourPasswords = SAMPLE_USERS.stream().map(u -> new User(u.getFirstName(), u.getLastName(), u.getUsername(), u.getPassword(), u.getRole(), u.isActive()))
+                .collect(Collectors.toList());
+        assertThat(users).hasSameElementsAs(usersWithourPasswords);
+
+        then(userRepository).should(times(1)).findAll();
+    }
+
+    @Test
+    void giveNewUser_whenUserPost_thenStatus201andLocationHeader() throws Exception {
+        given(userRepository.insert(any(User.class))).willReturn(newUser);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/users")
+                    .with(user("admin").password("admin123").roles(ADMIN.toString()))
+                    .accept(MediaType.APPLICATION_JSON)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(mapper.writeValueAsString(newUser)))
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(header().string("location",
+                        containsString("http://localhost/api/users/" + newUser.getId())))
+                .andExpect(jsonPath("$.username").value(newUser.getUsername()))
+                .andDo(result -> log.info(result.getResponse().getContentAsString()));
+
+    }
+
+    private static final List<User> SAMPLE_USERS = List.of(
+            new User("Default", "Admin", "admin", "admin123", ADMIN, true),
+            new User("Default", "User", "user", "user1234", USER, true),
+            new User("Ivan", "Petrov", "ivan", "ivan1234")
+    );
+
+    private static final User newUser = new User("60d1930a576619366bf00b20","Hristo", "Dimitrov", "hristo", "hristo123", USER, true);
+
 
 }
