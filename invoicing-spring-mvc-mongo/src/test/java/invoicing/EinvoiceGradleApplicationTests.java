c6c87b349195348a473a38f3b2ec70ac7cd2199f
Trayan Iliev <office@iproduct.org>
2021-06-22 11:34:19 +0300

    testing dependencies configured

--- invoicing-spring-mvc-mongo/src/test/java/invoicing/EinvoiceGradleApplicationTests.java
+++ invoicing-spring-mvc-mongo/src/test/java/invoicing/EinvoiceGradleApplicationTests.java
@@ -1,13 +1,21 @@
 package invoicing;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+        classes = EinvoiceGradleApplication.class)
+@AutoConfigureMockMvc
+@Slf4j
 class EinvoiceGradleApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
