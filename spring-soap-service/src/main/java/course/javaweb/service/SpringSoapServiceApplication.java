f3245c7ee45d77db653046655ea68ee16ce45322
Trayan Iliev <office@iproduct.org>
2021-06-22 15:36:27 +0300

    soap service initial setup

---
+++ spring-soap-service/src/main/java/course/javaweb/service/SpringSoapServiceApplication.java
@@ -0,0 +1,13 @@
+package course.javaweb.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSoapServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSoapServiceApplication.class, args);
+	}
+
+}
---
+++ spring-soap-service/src/test/java/course/javaweb/service/SpringSoapServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package course.javaweb.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSoapServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
